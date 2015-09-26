c038cb3bd1f8dd02a78f803f101aac2a0840d3e4
MiyamotoAkira <yefo.akira@gmail.com>
2015-09-26 09:49:23 +0100

    Added the tests for the validation of input

---
+++ tests/game_engine_tests.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative '../lib/game_engine'
+
+class GameEngineTest < Minitest::Test
+  def test_validate_input_no_valid_is_false
+    engine = GameEngine.new
+    assert_equal false, engine.validate_input('d')
+  end
+
+  def test_validate_input_more_than_one_character_is_false
+    engine = GameEngine.new
+    assert_equal false, engine.validate_input('09')
+  end
+
+  def test_validate_input_valid_number_is_true
+    engine = GameEngine.new
+    assert_equal true, engine.validate_input('9')
+  end
+end
