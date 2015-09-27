4f9d8dc055bad1016075fd09b9afd990060ad097
MiyamotoAkira <yefo.akira@gmail.com>
2015-09-27 22:13:27 +0100

    Created the medium and easy versions of the computer player

---
+++ lib/computer_strategy_easy.rb
@@ -0,0 +1,58 @@
+# This is an easy difficulty  to beat strategy for a computer controlled player
+# It will try to score at the first option, disregarding the actions of the player.
+# Unlike the hard strategy it will not try to fork. Unlike the medium strategy it will not block
+# It expects to yield to a block that will check if the game is finished or not
+# For that yield it will pass the state of the board after a possibe move has been chosen
+class ComputerStrategyEasy
+  def get_move(board, next_player, com, hum)
+    available_spaces = []
+    board.each do |s|
+      if s != com && s != hum
+        available_spaces << s
+      end
+    end
+
+    possible_move = check_possible_score(board, com, available_spaces, &Proc.new)
+
+    return possible_move if possible_move
+
+    possible_move = check_threatening(board, com, available_spaces, &Proc.new)
+
+    return possible_move if possible_move
+    
+    n = rand(0..available_spaces.count)
+    return available_spaces[n].to_i
+
+  end
+
+  def check_threatening(board, player, available_spaces)
+    available_spaces.each do |as|
+      left_to_assign = available_spaces - [as]
+      board[as.to_i] = player
+      left_to_assign.each do |cell|
+        board[cell.to_i] = player
+        if yield board
+          board[cell.to_i] = cell
+          return as.to_i
+        end
+        board[cell.to_i] = cell
+      end
+
+      board[as.to_i] = as
+    end
+    nil
+  end    
+  
+  def check_possible_score(board, player, available_spaces)
+    available_spaces.each do |as|
+      board[as.to_i] = player
+      if yield board
+        board[as.to_i] = as
+        return as.to_i
+      else
+        board[as.to_i] = as
+      end
+    end
+    nil     
+  end
+end
---
+++ tests/computer_strategy_easy_tests.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require_relative 'test_helper'
+require_relative '../lib/game_engine'
+require_relative '../lib/computer_strategy_easy'
+
+class ComputerStrategyEasyTest < Minitest::Test
+  
+  def test_get_move_first_move_no_center_random_chosen
+    test_board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    computer = ComputerStrategyMedium.new
+    assert_includes [0,1,2,3,4,5,6,7,8], computer.get_move(test_board, 'O', 'O', 'X') { |board| false}
+  end
+
+  def test_get_move_second_move_no_center_random_chosen
+    test_board = ["0", "1", "X", "3", "4", "5", "6", "7", "8"]
+    computer = ComputerStrategyMedium.new
+    assert_includes [0,1,3,4,5,6,7,8], computer.get_move(test_board, 'O', 'O', 'X') { |board| false}
+  end
+
+  def test_get_move_second_move_center_used_random_chosen
+    test_board = ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
+    computer = ComputerStrategyMedium.new
+    assert_includes [0,1,2,3,5,6,7,8], computer.get_move(test_board, 'O', 'O', 'X') { |board| false}
+  end
+
+  def test_get_move_player_is_ready_to_score_random_chosen
+    test_board = ["X", "1", "O", "3", "X", "5", "6", "7", "8"]
+    computer = ComputerStrategyMedium.new
+    engine = GameEngine.new
+    assert_includes [1,3,5,6,7,8], computer.get_move(test_board, 'O','O', 'X') {|board| engine.game_is_over(board)}
+  end
+
+  def test_get_move_computer_is_ready_to_score_score
+    test_board = ["X", "X", "O", "3", "X", "5", "6", "7", "O"]
+    computer = ComputerStrategyMedium.new
+    engine = GameEngine.new
+    assert_equal 5, computer.get_move(test_board, 'O','O', 'X') {|board| engine.game_is_over(board)}
+  end
+
+  def test_get_move_player_no_threatening_no_fork_available_use_threatening
+    test_board = ["O", "1", "2", "3", "X", "5", "6", "7", "8"]
+    computer = ComputerStrategyMedium.new
+    engine = GameEngine.new
+    assert_includes [0,1,2,3,5,6,7,8], computer.get_move(test_board, 'O','O', 'X') {|board| engine.game_is_over(board)}
+  end
+end
