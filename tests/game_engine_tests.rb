0f9bd26ad8c3945fe72487b5e19f8e025e972b18
MiyamotoAkira <yefo.akira@gmail.com>
2015-09-30 12:58:05 +0100

    Added a test for change_player

--- tests/game_engine_tests.rb
+++ tests/game_engine_tests.rb
@@ -13,4 +13,24 @@ class GameEngineTest < Minitest::Test
     engine = GameEngine.new(mock, mock, @marks, mock)
     assert_equal true, engine.check_board_spot_availability(0)
   end
+
+  def test_change_player_first_player
+    player1 = Object.new
+    player2 = Object.new
+    mock = Minitest::Mock.new
+    engine = GameEngine.new(player1, player2, @marks, mock)
+    result_player, result_player_mark = engine.change_player true
+    assert_same player1, result_player
+    assert_equal @marks[:player1_mark], result_player_mark
+  end
+
+  def test_change_player_not_first_player
+    player1 = Object.new
+    player2 = Object.new
+    mock = Minitest::Mock.new
+    engine = GameEngine.new(player1, player2, @marks, mock)
+    result_player, result_player_mark = engine.change_player false
+    assert_same player2, result_player
+    assert_equal @marks[:player2_mark], result_player_mark
+  end
 end
