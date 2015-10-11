24edc2e9b5bc70be7ad495aa8573eed1c27bf35c
MiyamotoAkira <yefo.akira@gmail.com>
2015-10-11 12:13:46 +0100

    Refactorings on Board
    Quite a few refactorings done on the board class. The main important point is that it now supports different board sizes. To account for it, a few methods have now been generalized. Some "code cleanliness" has been done to make the code slightly more readable.

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,21 @@ class Board
   
   def initialize player1_mark, player2_mark, size = 3
     @size = size
-    @upper_left_corner = 0
-    @upper_right_corner = 2
-    @lower_left_corner = 6
-    @lower_right_corner = 8
-    @board_positions = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    setup_board
     @player1_mark = player1_mark
     @player2_mark = player2_mark
+  end
+
+  def setup_board
+    @board_positions = []
+    (0...(@size*@size)).each do |position|
+      @board_positions << position.to_s
+    end
+
+    @upper_left_corner = 0
+    @upper_right_corner = @size - 1
+    @lower_left_corner = (@size * @size) - @size
+    @lower_right_corner = (@size * @size) - 1
   end
 
   def check_board_spot_availability(spot)
@@ -21,16 +29,69 @@ class Board
   end
 
   def at_least_one_line_the_same
-    [@board_positions[0], @board_positions[1], @board_positions[2]].uniq.length == 1 ||
-    [@board_positions[3], @board_positions[4], @board_positions[5]].uniq.length == 1 ||
-    [@board_positions[6], @board_positions[7], @board_positions[8]].uniq.length == 1 ||
-    [@board_positions[0], @board_positions[3], @board_positions[6]].uniq.length == 1 ||
-    [@board_positions[1], @board_positions[4], @board_positions[7]].uniq.length == 1 ||
-    [@board_positions[2], @board_positions[5], @board_positions[8]].uniq.length == 1 ||
-    [@board_positions[0], @board_positions[4], @board_positions[8]].uniq.length == 1 ||
-    [@board_positions[2], @board_positions[4], @board_positions[6]].uniq.length == 1
+    check_unique_horizontal || check_unique_vertical || check_unique_diagonal
   end
 
+  def check_unique_horizontal
+    (0...@size).each do |line|
+      positions = get_positions_for_line line
+      all_equal = check_for_uniqueness_on_positions positions
+      return all_equal if all_equal
+    end
+    false
+  end
+
+  def check_for_uniqueness_on_positions positions
+    all_equal = true
+    positions.each_with_index do |position, index|
+      if index != 0
+        all_equal = all_equal && (@board_positions[position] == @board_positions[positions[index - 1]])
+      end
+    end
+    all_equal
+  end
+  
+  def check_unique_vertical
+    (0...@size).each do |column|
+      positions = get_positions_for_column column
+      all_equal = check_for_uniqueness_on_positions positions
+      return all_equal if all_equal
+    end
+    false
+  end
+
+  def get_positions_for_line line
+    positions = []
+    (0...@size).each do |column|
+      positions << (line * @size) + column
+    end
+    positions
+  end
+  
+  def get_positions_for_column column
+    positions = []
+    (0...@size).each do |line|
+      positions << (line * @size) + column
+    end
+    positions
+  end
+
+  def get_positions_for_diagonal
+    positions = []
+    (0...@size).each_with_index do |line, index|
+      positions << (line * @size) + yield(index)
+    end
+    positions
+  end
+  
+  def check_unique_diagonal
+    positions = get_positions_for_diagonal {|index| index}
+    all_equal = check_for_uniqueness_on_positions positions
+    return all_equal if all_equal
+    positions = get_positions_for_diagonal {|index| (@size - index - 1) }
+    check_for_uniqueness_on_positions positions
+  end
+    
   def tie
     @board_positions.all? { |s| s == @player1_mark || s == @player2_mark }
   end
--- tests/board_tests.rb
+++ tests/board_tests.rb
@@ -80,4 +80,115 @@ class BoardTest < MiniTest::Test
     board = Board.new @player1_mark, @player2_mark
     assert_equal :empty, board.check_lower_right_corner
   end
+
+  def test_setup_board_size_3
+    size = 3
+    board = Board.new @player1_mark, @player2_mark, size
+    assert_equal 9, board.board_positions.length
+    assert_equal 0, board.upper_left_corner
+    assert_equal 2, board.upper_right_corner
+    assert_equal 6, board.lower_left_corner
+    assert_equal 8, board.lower_right_corner
+  end
+
+  def test_setup_board_size_4
+    size = 4
+    board = Board.new @player1_mark, @player2_mark, size
+    assert_equal 16, board.board_positions.length
+    assert_equal 0, board.upper_left_corner
+    assert_equal 3, board.upper_right_corner
+    assert_equal 12, board.lower_left_corner
+    assert_equal 15, board.lower_right_corner
+  end
+
+  def test_check_unique_horizontal_no_equals
+    board = Board.new @player1_mark, @player2_mark
+    assert_equal false, board.check_unique_horizontal
+  end
+
+  def test_check_unique_horizontal_first_horizontal
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 0, 'X'
+    board.occupy_spot 1, 'X'
+    board.occupy_spot 2, 'X'
+    assert_equal true, board.check_unique_horizontal
+  end
+
+  def test_check_unique_horizontal_second_horizontal
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 3, 'X'
+    board.occupy_spot 4, 'X'
+    board.occupy_spot 5, 'X'
+    assert_equal true, board.check_unique_horizontal
+  end  
+
+  def test_check_unique_horizontal_size_4_second_horizontal
+    board = Board.new @player1_mark, @player2_mark, 4
+    board.occupy_spot 4, 'X'
+    board.occupy_spot 5, 'X'
+    board.occupy_spot 6, 'X'
+    board.occupy_spot 7, 'X'
+    assert_equal true, board.check_unique_horizontal
+  end
+
+  def test_check_unique_vertical_no_equals
+    board = Board.new @player1_mark, @player2_mark
+    assert_equal false, board.check_unique_vertical
+  end
+
+  def test_check_unique_vertical_first_vertical
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 0, 'X'
+    board.occupy_spot 3, 'X'
+    board.occupy_spot 6, 'X'
+    assert_equal true, board.check_unique_vertical
+  end
+
+  def test_check_unique_vertical_second_vertical
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 1, 'X'
+    board.occupy_spot 4, 'X'
+    board.occupy_spot 7, 'X'
+    assert_equal true, board.check_unique_vertical
+  end  
+
+  def test_check_unique_vertical_size_4_second_vertical
+    board = Board.new @player1_mark, @player2_mark, 4
+    board.occupy_spot 1, 'X'
+    board.occupy_spot 5, 'X'
+    board.occupy_spot 9, 'X'
+    board.occupy_spot 13, 'X'
+    assert_equal true, board.check_unique_vertical
+  end
+
+  def test_check_unique_diagonal_no_equals
+    board = Board.new @player1_mark, @player2_mark
+    assert_equal false, board.check_unique_diagonal
+  end
+
+  def test_check_unique_diagonal_first_diagonal
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 0, 'X'
+    board.occupy_spot 4, 'X'
+    board.occupy_spot 8, 'X'
+    assert_equal true, board.check_unique_diagonal
+  end
+
+  def test_check_unique_diagonal_second_diagonal
+    board = Board.new @player1_mark, @player2_mark
+    board.occupy_spot 2, 'X'
+    board.occupy_spot 4, 'X'
+    board.occupy_spot 6, 'X'
+    assert_equal true, board.check_unique_diagonal
+  end  
+
+  def test_check_unique_diagonal_size_4_second_diagonal
+    board = Board.new @player1_mark, @player2_mark, 4
+    board.occupy_spot 3, 'X'
+    board.occupy_spot 6, 'X'
+    board.occupy_spot 9, 'X'
+    board.occupy_spot 12, 'X'
+    assert_equal true, board.check_unique_diagonal
+  end
+  
 end
